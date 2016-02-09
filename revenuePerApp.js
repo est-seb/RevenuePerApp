ff07568ca7a2a198a977b30dc15b9fae598a510d
Seb Estournes <est.seb@gmail.com>
2016-02-09 19:23:40 +0800

    Corrected bugs : set withholding tax and beginning balance to float, put the message of missing currency rate at the right time. And reset variables when 1 set of file has been processed.

--- revenuePerApp.js
+++ revenuePerApp.js
@@ -86,12 +86,12 @@ var revenuePerApp = {
               if (app_LocalCurrencyShareMap.hasOwnProperty(appName)) {
                 //Withholding tax
                 if (withholdingTaxMap!={} && withholdingTaxMap.hasOwnProperty(localCurrencyCode)){
-                  app_LocalCurrencyShareMap[appName] = app_LocalCurrencyShareMap[appName] + withholdingTaxMap[localCurrencyCode];
+                  app_LocalCurrencyShareMap[appName] = app_LocalCurrencyShareMap[appName] + parseFloat(withholdingTaxMap[localCurrencyCode]);
                   console.log("Withholding tax : "+app_LocalCurrencyShareMap[appName]);
                 }
                 //beginning balance
                 if (beginningBalanceMap!={} && beginningBalanceMap.hasOwnProperty(localCurrencyCode)){
-                  app_LocalCurrencyShareMap[appName] = app_LocalCurrencyShareMap[appName] + beginningBalanceMap[localCurrencyCode];
+                  app_LocalCurrencyShareMap[appName] = app_LocalCurrencyShareMap[appName] + parseFloat(beginningBalanceMap[localCurrencyCode]);
                   console.log("Beginning balance : "+app_LocalCurrencyShareMap[appName]);
                 }
               }
@@ -105,60 +105,76 @@ var revenuePerApp = {
         }
 
         //Currency rates are necessary for the rest of the calculation
-        if (nbFiles==nbFilesProcessed && currencyCode_currencyRate!=={}) {
-          console.log("Last file processed");
-          //Calculation of shares in the targeted currency
-          for (var currencyCode in currencyCode_AppLocalCurrency) {
-            if (currencyCode_AppLocalCurrency.hasOwnProperty(currencyCode)) {
-              var app_TargetCurrencyShareMap = {};
-              console.log(currencyCode_AppLocalCurrency[currencyCode]);
-              app_LocalCurrencyShareMap = currencyCode_AppLocalCurrency[currencyCode];
-              for (var appName in app_LocalCurrencyShareMap) {
-                if (app_LocalCurrencyShareMap.hasOwnProperty(appName)) {
-                  //Get currency rate according to currency code
-                  var rate = parseFloat(currencyCode_currencyRate[currencyCode]);
+        if (nbFiles==nbFilesProcessed) {
+          if (currencyCode_currencyRate!=={}) {
+            //Calculation of shares in the targeted currency
+            for (var currencyCode in currencyCode_AppLocalCurrency) {
+              if (currencyCode_AppLocalCurrency.hasOwnProperty(currencyCode)) {
+                var app_TargetCurrencyShareMap = {};
+                console.log(currencyCode_AppLocalCurrency[currencyCode]);
+                app_LocalCurrencyShareMap = currencyCode_AppLocalCurrency[currencyCode];
+                for (var appName in app_LocalCurrencyShareMap) {
+                  if (app_LocalCurrencyShareMap.hasOwnProperty(appName)) {
+                    //Get currency rate according to currency code
+                    var rate = parseFloat(currencyCode_currencyRate[currencyCode]);
 
-                  var shareInTargetedCurrency = parseFloat(app_LocalCurrencyShareMap[appName]) * rate;
-                  app_TargetCurrencyShareMap[appName] = shareInTargetedCurrency;
+                    var shareInTargetedCurrency = parseFloat(app_LocalCurrencyShareMap[appName]) * rate;
+                    app_TargetCurrencyShareMap[appName] = shareInTargetedCurrency;
 
-                  if (app_TargetedCurrencyTotal.hasOwnProperty(appName)) {
-                    app_TargetedCurrencyTotal[appName] = parseFloat(app_TargetedCurrencyTotal[appName]) + shareInTargetedCurrency;
-                  } else {
-                    app_TargetedCurrencyTotal[appName] = shareInTargetedCurrency;
+                    if (app_TargetedCurrencyTotal.hasOwnProperty(appName)) {
+                      app_TargetedCurrencyTotal[appName] = parseFloat(app_TargetedCurrencyTotal[appName]) + shareInTargetedCurrency;
+                    } else {
+                      app_TargetedCurrencyTotal[appName] = shareInTargetedCurrency;
+                    }
                   }
                 }
+                //Store apps total shares in targeted currency in global map
+                currencyCode_AppTargetedCurrency[currencyCode] = app_TargetCurrencyShareMap;
               }
-              //Store apps total shares in targeted currency in global map
-              currencyCode_AppTargetedCurrency[currencyCode] = app_TargetCurrencyShareMap;
             }
-          }
 
-          //Reset counter for futur use in the same session
-          nbFilesProcessed = 0;
+            //Reset counter for futur use in the same session
+            nbFilesProcessed = 0;
 
-          //display results
-          var resultsText = "<p>Total revenue per app : </p>";
-          var totalRevenue = 0;
-          var revenueOfOneApp = 0;
-          for (var appName in app_TargetedCurrencyTotal) {
-            if (app_TargetedCurrencyTotal.hasOwnProperty(appName)) {
-              revenueOfOneApp = parseFloat(app_TargetedCurrencyTotal[appName]);
-              //Round up at 2 decimals
-              revenueOfOneApp = Math.round(revenueOfOneApp * 100)/100;
-              resultsText = resultsText + appName + " : " + revenueOfOneApp + "</BR>";
-              totalRevenue = Math.round((parseFloat(totalRevenue) + parseFloat(revenueOfOneApp)) * 100)/100;
+            //display results
+            var resultsText = "<p>Total revenue per app : </p>";
+            var totalRevenue = 0;
+            var revenueOfOneApp = 0;
+            for (var appName in app_TargetedCurrencyTotal) {
+              if (app_TargetedCurrencyTotal.hasOwnProperty(appName)) {
+                revenueOfOneApp = parseFloat(app_TargetedCurrencyTotal[appName]);
+                //Round up at 2 decimals
+                revenueOfOneApp = Math.round(revenueOfOneApp * 100)/100;
+                resultsText = resultsText + appName + " : " + revenueOfOneApp + "</BR>";
+                totalRevenue = Math.round((parseFloat(totalRevenue) + parseFloat(revenueOfOneApp)) * 100)/100;
+              }
             }
+            resultsText = resultsText + "<p>Total Revenue For This Month : " + totalRevenue + "</p>"
+            document.getElementById("results").innerHTML = resultsText;
+            document.getElementById("results").style.display = 'block';
+            console.log(currencyCode_AppTargetedCurrency);
+            console.log(app_TargetedCurrencyTotal);
+
+            revenuePerApp.resetMaps();
+          } else {
+            document.getElementById("results").innerHTML = NO_CURRENCY_RATE_FILE_FOUND;
+            document.getElementById("results").style.display = 'block';
           }
-          resultsText = resultsText + "<p>Total Revenue For This Month : " + totalRevenue + "</p>"
-          document.getElementById("results").innerHTML = resultsText;
-          document.getElementById("results").style.display = 'block';
-          console.log(currencyCode_AppTargetedCurrency);
-          console.log(app_TargetedCurrencyTotal);
-        } else {
-          document.getElementById("results").innerHTML = NO_CURRENCY_RATE_FILE_FOUND;
-          document.getElementById("results").style.display = 'block';
         }
     });
+  },
+
+  resetMaps : function(){
+    currencyRateFileFound = false;
+    nbFiles = 0;
+    nbFilesProcessed = 0;
+    currencyCode_AppLocalCurrency = {};
+    currencyCode_AppTargetedCurrency = {};
+    currencyCode_currencyRate = {};
+    app_TargetedCurrencyTotal = {};
+
+    beginningBalanceMap = {};
+    withholdingTaxMap = {};
   },
 
   getArrayOfLines : function(wholeText){
